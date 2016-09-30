64da09546c610bc7a20aa447d3a069caaf64264f
Wopsie <joaghim.beltman@gmail.com>
2016-09-30 13:31:48 +0200

    tinkered, mobile test

--- js/Start.js
+++ js/Start.js
@@ -30,8 +30,8 @@ var start = {
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;            
 		this.scale.minWidth = worldWidth/2;            
 		this.scale.minHeight = worldHeight/2;            
-		this.scale.maxWidth = worldWidth*(window.screen.availWidth / window.innerWidth);            
-		this.scale.maxHeight = worldHeight* (window.screen.availHeight / window.innerHeight);            
+		this.scale.maxWidth = worldWidth*(window.screen.availWidth);            
+		this.scale.maxHeight = worldHeight* (window.screen.availHeight);            
 		this.scale.pageAlignHorizontally = true;            
 		this.scale.pageAlignVertically = true;            
 		this.scale.setScreenSize(true);
