e485d05af3c60340fa93ec39509abf9e29965cee
Wopsie <joaghim.beltman@gmail.com>
2016-09-30 22:20:59 +0200

    bug testing

--- js/Food.js
+++ js/Food.js
@@ -7,12 +7,14 @@ var food = {
 		this.RNG();
 		//store food sprite in variable for global use
 		pickup = game.add.sprite(randomXPos, randomYPos,'foodSprite');
+
+		console.log('SPAWN FOOD');
 	},
 
 	RNG : function()
 	{
 		//generate random X and Y Coordinate
 		randomXPos = Math.floor(Math.random() * 40 ) * square;
-		randomYPos = Math.floor(Math.random() * 30 ) * square;
+		randomYPos = Math.floor(Math.random() * 28 ) * square;
 	},
 }
